cc92374aa99ef2e87e1508103783cc967fad78f6
Aryawedha <gedearyawedha@unud.ac.id>
2024-09-09 19:53:56 +0800

    latihan

---
+++ soal1.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int angka = 2;
+    
+    if(angka % 2 == 0){
+        printf("Genap");
+    }
+    else{
+        printf("Ganjil");
+    }
+}
